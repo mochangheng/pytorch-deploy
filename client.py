db260fb7d3be76bb83c772593e3db5882798389b
Chang Heng Mo <mochangheng@gmail.com>
2020-08-24 16:41:00 -0400

    removing useless files

--- client.py
+++
@@ -1,9 +0,0 @@
-import requests
-import numpy as np
-
-filename = "examples/palm.jpg"
-files = {'file': open(filename, "rb")}
-r = requests.post("http://127.0.0.1:8000/predict_image", files=files)
-response = r.json()
-output = np.array(response["output"])
-print(np.argmax(output))
